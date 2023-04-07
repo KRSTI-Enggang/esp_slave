6d45c6d322062128d3f74685fc10be21f08bd452
Abdullah Baqir Arrafii <arrafiiabdullah@gmail.com>
2023-04-07 20:08:45 +0800

    Update esp_slave.ino

--- esp_slave.ino
+++ esp_slave.ino
@@ -1,9 +1,37 @@
-void setup() {
-  // put your setup code here, to run once:
+//Slave untuk ke master
+#include "BluetoothSerial.h"
 
+const char *pin = "1234"; //ubah pin dan samakan dengan master
+
+String device_name = "Enggang-SLAVE";
+
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
+#if !defined(CONFIG_BT_SPP_ENABLED)
+#error Serial Bluetooth not available or not enabled. It is only available for the ESP32 chip.
+#endif
+
+BluetoothSerial SerialBT;
+
+void setup() {
+  Serial.begin(115200);
+  SerialBT.begin(device_name); //Nama perangkat
+  Serial.printf("Perangkat dimulai dengan nama \"%s\"siap pairing!\n", device_name.c_str());
+  //Serial.printf("The device with name \"%s\" and MAC address %s is started.\nNow you can pair it with Bluetooth!\n", device_name.c_str(), SerialBT.getMacString()); // Use this after the MAC method is implemented
+  #ifdef USE_PIN
+    SerialBT.setPin(pin);
+    Serial.println("Using PIN");
+  #endif
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  if (Serial.available()) {
+    SerialBT.write(Serial.read());
+  }
+  if (SerialBT.available()) {
+    Serial.write(SerialBT.read());
+  }
+  delay(20);
 }
